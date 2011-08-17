27b4d163735a851cc7eba9a4b0dea65e1023eb02
Petr Bělohlávek <petr.bel@centrum.cz>
2011-08-17 13:20:35 +0200

    login form development

--- app/FrontModule/presenters/DefaultPresenter.php
+++ app/FrontModule/presenters/DefaultPresenter.php
@@ -8,10 +8,18 @@ class DefaultPresenter extends BasePresenter
 	protected function createComponentLoginForm()
 	{
 		$form = new Nette\Application\UI\Form;
-		$form->addText('name', 'Jméno');
-		$form->addPassword('password', 'Heslo');
+
+		$form->addText('login', 'Login')
+			->setRequired('Zadejte prosím přihlašovací jméno');
+
+		$form->addPassword('password', 'Heslo')
+			->setRequired('Zadejte prosím heslo');
+		
 		$form->addSubmit('send', 'Přihlásit');
+
 		//$form->setTranslator($translator);
+		//$form->setAction('/submit.php');
+		//$form->setMethod('POST');
 		
 		return $form;
 	}
