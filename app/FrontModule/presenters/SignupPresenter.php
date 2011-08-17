e994039e4a41171a06c5131abf79d4e14fecc330
Petr Bělohlávek <petr.bel@centrum.cz>
2011-08-17 21:20:53 +0200

    signup form changed

--- app/FrontModule/presenters/SignupPresenter.php
+++ app/FrontModule/presenters/SignupPresenter.php
@@ -19,49 +19,29 @@ class SignupPresenter extends BasePresenter
 		
 		$form = new Form;
 
-		$form->addGroup("Osobní");
-			$form->addText('name', 'Jméno')
-				->setRequired('Zadejte prosím jméno');
+		$form->addText('login', 'Login')
+			->setRequired('Zadejte prosím unikátní login');
 
-			$form->addText('age', 'Věk')
-				->addRule(Form::INTEGER, 'Věk musí být číslo')
-				->addRule(Form::RANGE, 'Věk musí být od %d do %d let', array(7, 101));
+		$form->addText('email', 'E-mail')
+			->setRequired('Zadejte prosím platný e-mail')
+			->addRule(Form::EMAIL, 'Neplatná e-mailová adresa');
 
-			$sex = array(
-				'm' => 'muž',
-				'f' => 'žena',
-			);
-			$form->addRadioList('gender', 'Pohlaví:', $sex);
+		$form->addPassword('password', 'Heslo')
+			->setRequired('Zadejte prosím heslo')
+			->addRule(Form::MIN_LENGTH, 'Heslo musí mít alespoň %d znaků', 5);
 
-		$form->addGroup("Hra");
-			$form->addText('login', 'Login')
-				->setRequired('Zadejte prosím unikátní login');
-				
-			$form->addText('email', 'E-mail')
-				->setRequired('Zadejte prosím platný e-mail')
-				->addRule(Form::EMAIL, 'Neplatná e-mailová adresa');
-				
-			$form->addPassword('password', 'Heslo')
-				->setRequired('Zadejte prosím heslo')
-				->addRule(Form::MIN_LENGTH, 'Heslo musí mít alespoň %d znaků', 5);
-				
-			$form->addPassword('passwordVerify', 'Heslo (pro kontrolu)')
-				->setRequired('Zadejte prosím heslo ještě jednou pro kontrolu')
-				->addRule(Form::EQUAL, 'Hesla se neshodují', $form['password']);		
-				
-			/*$form->addText('landName', 'Jméno země')
-				->setRequired('Zadejte prosím unikátní jméno země');
-*/
-		$form->addGroup("Podmínky");
+		$form->addPassword('passwordVerify', 'Heslo (pro kontrolu)')
+			->setRequired('Zadejte prosím heslo ještě jednou pro kontrolu')
+			->addRule(Form::EQUAL, 'Hesla se neshodují', $form['password']);
 
-			$form->addTextArea("conditions", "Podmínky")
-				->setDefaultValue("lorem \nipsum \ndolor \nsit...\n ahoj\n ahoj\n ahoj\n ahoj\n ahoj\n ahoj\n ahoj\n ahojahoj\n ahoj\nahoj\n ahoj\nahoj\n ahoj\n")
-				->setDisabled();
-			
-			$form->addCheckbox("agree", "Souhlasím s podmínkami")
-				->addRule(Form::EQUAL, 'Je potřeba souhlasit s podmínkami', TRUE);
-			
-			$form->addSubmit('send', 'Registrovat');
+		$form->addTextArea("conditions", "Podmínky")
+			->setDefaultValue("lorem \nipsum \ndolor \nsit...\n ahoj\n ahoj\n ahoj\n ahoj\n ahoj\n ahoj\n ahoj\n ahojahoj\n ahoj\nahoj\n ahoj\nahoj\n ahoj\n")
+			->setDisabled();
+
+		$form->addCheckbox("agree", "Souhlasím s podmínkami")
+			->addRule(Form::EQUAL, 'Je potřeba souhlasit s podmínkami', TRUE);
+
+		$form->addSubmit('send', 'Registrovat');
 
 		//$form->setTranslator($translator);
 		
