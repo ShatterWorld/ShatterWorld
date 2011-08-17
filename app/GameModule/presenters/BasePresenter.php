90c88ff9657b231fd85d40949639610864e9a548
Jan Buchar <Teyras@gmail.com>
2011-08-17 23:49:34 +0200

    fixes

--- app/GameModule/presenters/BasePresenter.php
+++ app/GameModule/presenters/BasePresenter.php
@@ -4,11 +4,19 @@ use Nette;
 
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
-
-		public function handleLogout(){
-			$user = $this->getUser();
-			$user->logout();
-			$this->flashMessage("Odhlášen");
-			$this->redirect(':Front:Default:');
+	public function startup ()
+	{
+		parent::startup();
+		if (!$this->getUser()->isLoggedIn()) {
+			$this->redirect(':Front:Homepage:');
 		}
+	}
+	
+	public function handleLogout () 
+	{
+		$user = $this->getUser();
+		$user->logout();
+		$this->flashMessage("Odhlášen");
+		$this->redirect(':Front:Homepage:');
+	}
 }
