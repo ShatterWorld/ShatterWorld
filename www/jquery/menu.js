d074dd64c3550c3af468d06b6f1cb51b7a48b021
Petr Bělohlávek <petr.bel@centrum.cz>
2011-08-18 19:56:15 +0200

    further submenu development

--- www/jquery/menu.js
+++ www/jquery/menu.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-  $(".header").click(function(){
-	$(".submenu ul").hide("fast");
-    $(this).parent().children("ul").show("fast");
-  });
+	$("#menu").children("div:first-child").children("ul").show("slow"); /*displays the firth submenu*/
+	
+	$(".header").click(function(){ /*toogles visibility*/
+		$(".submenu ul").hide("fast");
+ 		$(this).parent().children("ul").show("fast");
+	});
 });
