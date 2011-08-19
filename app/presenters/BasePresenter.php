0f48d759678f371a4e36d9c808a6e586565dd0f5
Jan Buchar <Teyras@gmail.com>
2011-08-19 11:05:15 +0200

    Dynamic Service/Repository access from the Presenter

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -12,7 +12,11 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 	public function __call ($name, $args)
 	{
 		if (Strings::startsWith($name, 'get')) {
-			
+			if (Strings::endsWith($name, 'Service')) {
+				return $this->context->getService(lcfirst(substr($name, 3)));
+			} elseif (Strings::endsWith($name, 'Repository')) {
+				return $this->context->getService(lcfirst(substr($name, 3, -10)) . 'Service')->getRepository();
+			}
 		}
 		return parent::__call($name, $args);
 	}
