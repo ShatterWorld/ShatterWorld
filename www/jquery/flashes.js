b2ec0b85dd4727b002b4344fc91e4e8c45ac06a3
Jan Buchar <Teyras@gmail.com>
2011-08-19 20:41:17 +0200

    moved jquery->js

--- www/jquery/flashes.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("#menu").children("div:first-child").children("ul").show("slow"); /*displays the firth submenu*/
-	
-	$("#flashes .flash").click(function(){ /*toogles visibility*/
-		$(this).hide("fast");
-	});
-});
--- www/jquery/menu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#menu").children("div:first-child").children("ul").show("slow"); /*displays the firth submenu*/
-	
-	$(".header").click(function(){ /*toogles visibility*/
-		$(".submenu ul").hide("fast");
- 		$(this).parent().children("ul").show("fast");
-	});
-});
