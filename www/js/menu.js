8fcf45040b1c1ab094c03c09d20d1902aa907563
Petr Bělohlávek <petr.bel@centrum.cz>
2011-08-25 10:44:33 +0200

    menu development - possibility to click <li> to navigate (js and css)

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
+
+	/**
+	* Hides all submenus except the first one
+	*/
 	$("#menu .submenu ul").hide();
 	$("#menu").children("div:first-child").children("ul").show("slow"); /*displays the firth submenu*/
 	
-	$("#menu .submenu h3").click(function(){ /*toogles visibility*/
+	/**
+	* Toogles visibility of submenus
+	*/
+	$("#menu .submenu h3").click(function(){ 
 		$(this).parent().children("ul").toggle("fast");
+	});
+
+	/**
+	* Click the link even it user clicks only <li>
+	*/
+	$("#menu .submenu li").click(function(){ 
+		location.href = $(this).children("a").attr('href');
 	});
 });
