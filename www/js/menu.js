fef2e8d0420b957d6b9b0498409d40327520739d
Petr Bělohlávek <petr.bel@centrum.cz>
2011-08-27 15:30:19 +0200

    menu styling

--- www/js/menu.js
+++ www/js/menu.js
@@ -5,18 +5,12 @@ $(document).ready(function(){
 	*/
 	$("#menu .submenu ul").hide();
 	$("#menu").children("div:first-child").children("ul").show("slow"); /*displays the firth submenu*/
-	
+
 	/**
 	* Toogles visibility of submenus
 	*/
-	$("#menu .submenu h3").click(function(){ 
+	$("#menu .submenu h3").click(function(){
 		$(this).parent().children("ul").toggle("fast");
 	});
 
-	/**
-	* Click the link even it user clicks only <li>
-	*/
-	$("#menu .submenu li").click(function(){ 
-		location.href = $(this).children("a").attr('href');
-	});
 });
