38a261a9f77adf67e10f8d3ac783c12c93ff5b4d
Jan Buchar <Teyras@gmail.com>
2011-08-28 12:19:36 +0200

    DI refactoring

---
+++ app/services/ServiceFactory.php
@@ -0,0 +1,47 @@
+<?php
+class ServiceFactory extends Nette\Object
+{
+	public static function createEntityManager ($container)
+	{
+		$config = new Doctrine\ORM\Configuration;
+		$driver = $config->newDefaultAnnotationDriver($container->params['doctrine']['entityDir']);
+		$cache = $container->getService('doctrineCache');
+		$config->setMetadataDriverImpl($driver);
+		$config->setMetadataCacheImpl($cache);
+		$config->setQueryCacheImpl($cache);
+		$config->setProxyNamespace($container->params['doctrine']['proxyNamespace']);
+		$config->setProxyDir($container->params['doctrine']['proxyDir']);
+		$config->setAutoGenerateProxyClasses(!$container->params['productionMode']);
+		if (!$container->params['productionMode']) {
+			$config->setSQLLogger(\Nella\Doctrine\Panel::register());
+		}
+		$connection = $container->params['database'];
+		return EntityManager::create($connection, $config);
+	}
+	
+	public static function createAuthorizator ($container)
+	{
+		$acl = new Nette\Security\Permission;
+		$acl->addResource('administration');
+		$acl->addRole('player');
+		$acl->addRole('admin');
+		$acl->allow('admin');
+		return $acl;
+	}
+	
+	public static function createModelContainer ($container)
+	{
+		$context = new Container();
+		$context->lazyCopy($container, 'entityManager');
+		$context->lazyCopy($container, 'cacheStorage');
+		$context->lazyCopy($container, 'doctrineCache');
+		$context->lazyCopy($container, 'rules');
+		$context->params['game'] = &$container->params['game'];
+		$context->params['database'] = &$container->params['database'];
+		$context->params['doctrine'] = &$container->params['doctrine'];
+		$context->params['appDir'] = $container->params['appDir'];
+		$context->params['wwwDir'] = $container->params['wwwDir'];
+		$context->params['productionMode'] = $container->params['productionMode'];
+		return new ModelContainer($context);
+	}
+}
