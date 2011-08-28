9d69fc9a5c305df8a9ed22b6edc7d99039dd59d2
Petr Bělohlávek <petr.bel@centrum.cz>
2011-08-28 14:45:59 +0200

    map navig

--- www/js/map.js
+++ www/js/map.js
@@ -25,20 +25,20 @@ $(document).ready(function(){
 	* Data (indexed by html tags data-STH, e.g. data-coords -> index='coords')
 	*/
 	var data = null;
-	
+
 	var basepath = $('#map').data()['basepath'];
 	/**
 	* Array of marked fields
 	*/
 	var markedFieldsId = new Array();
-	
+
 	/**
 	* Array of marked fields data
 	*/
 	var markedFieldsColors = new Array();
-	
+
 	var markerImage = $('<img class="marker" />').attr('src', basepath + '/images/fields/marker.png');
-	
+
 	/**
 	* Shows and fills #fieldInfo and #fieldActions when user gets mouse over a field
 	* @return void
@@ -51,10 +51,10 @@ $(document).ready(function(){
 		$("#fieldInfo #coords").html('Souřadnice ['+data['coords'][0]+';'+data['coords'][1]+']');
 		$("#fieldInfo #owner").html('Vlastník '+data['owner']);
 		$("#fieldInfo #type").html('Typ '+data['type']);
-			
+
 	});
-	
-	
+
+
 	/**
 	* Hides #fieldInfo
 	* @return void
@@ -62,8 +62,8 @@ $(document).ready(function(){
 	$(".field").mouseleave(function(){
 		$('#fieldInfo').hide();
 	});
-	
-		
+
+
 	/**
 	* Moves with #infoBox
 	* @return void
@@ -77,7 +77,7 @@ $(document).ready(function(){
 		$('#fieldInfo').css("top", e.pageY + 20);
 	});
 
-	//});	
+	//});
 
 
 	/**
@@ -87,7 +87,7 @@ $(document).ready(function(){
 	$(".field").dblclick(function(){
 		$('#fieldDetail').hide();
 		$('#fieldDetail').show('fast');
-		
+
 		//$('#fieldImg').attr('src', '');
 		$('#fieldImg').attr('src', basepath+'/images/fields/hex_'+data['color']+'.png');
 
@@ -96,10 +96,10 @@ $(document).ready(function(){
 		$('#detailType').html(data['type']);
 		$('#detailFacility').html(data['facility']);
 		$('#detailLevel').html(data['level']);
-		
-		
-			
-	});	
+
+
+
+	});
 
 	/**
 	* Hides #fieldDetail
@@ -107,7 +107,7 @@ $(document).ready(function(){
 	*/
 	$('#closeFieldDetail').click(function(){
 		$('#fieldDetail').hide('fast');
-	});		
+	});
 
 
 	/**
@@ -117,29 +117,29 @@ $(document).ready(function(){
 	*/
 	$(".field").click(function(){
 
-		clicks++;		
+		clicks++;
 		mark(this);
 
 		if (clicks < 2)
 		{
 			clearActions();
 			prevField = this;
-		
+
 		}
 		else if ((clicks > 2) || (this == prevField))
 		{
 			unmarkAll();
 			clearActions();
 			prevField = null;
-		
+
 		}
 		else if (clicks == 2)
 		{
 			showMenu();
 			prevField = this;
 		}
-		
-				
+
+
 	});
 
 	/**
@@ -150,7 +150,7 @@ $(document).ready(function(){
 	{
 		$('#field_'+data['coords'][0]+'_'+data['coords'][1]).append(markerImage.clone());
 	}
-	
+
 	/**
 	* Unmarks all fields and sets click to zero
 	* @return void
@@ -160,7 +160,7 @@ $(document).ready(function(){
 		$('.marker').remove();
 		clicks = 0;
 	}
-	
+
 	/**
 	* Displays context menu
 	* @return void
@@ -170,19 +170,19 @@ $(document).ready(function(){
 		var x = data['realcoords'][0];
 		var y = data['realcoords'][1];
 //		alert('tmp');
-		
+
 		/*TODO: action-click clears actions and unmarks fields*/
-		
+
 		clearActions();
 		addAction('Útok<br/>');
 		addAction('Podpora<br/>');
 		addAction('Poslat suroviny<br/>');
-		
-		//$('#mapContainer').prepend('<div id="#contextMenu" style="background: url(\'../images/hex_marked.png\'); position: absolute; top: +'x'+; left: +'y'+; width: 50px; height:50px;">bla<br/>bla2<br/>bla3</div>');	
-		//alert('<div id="#contextMenu" style="background: url(\'../images/hex_marked.png\'); position: absolute; top: +'x'+; left: +'y'+; width: 50px; height:50px;">bla<br/>bla2<br/>bla3</div>');	
+
+		//$('#mapContainer').prepend('<div id="#contextMenu" style="background: url(\'../images/hex_marked.png\'); position: absolute; top: +'x'+; left: +'y'+; width: 50px; height:50px;">bla<br/>bla2<br/>bla3</div>');
+		//alert('<div id="#contextMenu" style="background: url(\'../images/hex_marked.png\'); position: absolute; top: +'x'+; left: +'y'+; width: 50px; height:50px;">bla<br/>bla2<br/>bla3</div>');
 	}
-	
-	
+
+
 	/**
 	* Sets global data (JSON) from object named e.g. data-coord
 	* @param object
@@ -192,7 +192,7 @@ $(document).ready(function(){
 	{
 		data = $(object).data();
 	}
-	
+
 	/**
 	* Clears #fieldActions
 	* @return void
@@ -201,7 +201,7 @@ $(document).ready(function(){
 	{
 		$('#fieldActions').html('');
 	}
-	
+
 	/**
 	* Clears #fieldActions
 	* @param action
@@ -211,5 +211,63 @@ $(document).ready(function(){
 	{
 		$('#fieldActions').append(action);
 	}
-	
+
+
+
+	/**
+	 * @var integer represents speed of navigation [ms]
+	 */
+	var navigSpeed = 400;
+
+	/**
+	 * @var integer how far the scroller moves [px]
+	 */
+	var slide = 250;
+
+
+	$('#navig #up').click(function(){
+			var pos = $('#mapContainer').scrollTop();
+			$('#mapContainer').animate({scrollTop: pos-slide}, navigSpeed);
+	});
+	$('#navig #down').click(function(){
+			var pos = $('#mapContainer').scrollTop();
+			$('#mapContainer').animate({scrollTop: pos+slide}, navigSpeed);
+	});
+
+	$('#navig #left').click(function(){
+			var pos = $('#mapContainer').scrollLeft();
+			$('#mapContainer').animate({scrollLeft: pos-slide}, navigSpeed);
+	});
+	$('#navig #right').click(function(){
+			var pos = $('#mapContainer').scrollLeft();
+			$('#mapContainer').animate({scrollLeft: pos+slide}, navigSpeed);
+	});
+
+	$('#navig #leftup').click(function(){
+			var posTop = $('#mapContainer').scrollTop();
+			$('#mapContainer').animate({scrollTop: posTop-slide}, navigSpeed);
+			var posLeft = $('#mapContainer').scrollLeft();
+			$('#mapContainer').animate({scrollLeft: posLeft-slide}, navigSpeed);
+	});
+	$('#navig #rightup').click(function(){
+			var posTop = $('#mapContainer').scrollTop();
+			$('#mapContainer').animate({scrollTop: posTop-slide}, navigSpeed);
+			var posLeft = $('#mapContainer').scrollLeft();
+			$('#mapContainer').animate({scrollLeft: posLeft+slide}, navigSpeed);
+	});
+
+	$('#navig #leftdown').click(function(){
+			var posTop = $('#mapContainer').scrollTop();
+			$('#mapContainer').animate({scrollTop: posTop+slide}, navigSpeed);
+			var posLeft = $('#mapContainer').scrollLeft();
+			$('#mapContainer').animate({scrollLeft: posLeft-slide}, navigSpeed);
+	});
+	$('#navig #rightdown').click(function(){
+			var posTop = $('#mapContainer').scrollTop();
+			$('#mapContainer').animate({scrollTop: posTop+slide}, navigSpeed);
+			var posLeft = $('#mapContainer').scrollLeft();
+			$('#mapContainer').animate({scrollLeft: posLeft+slide}, navigSpeed);
+	});
+
+
 });
