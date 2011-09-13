9840b99ab780d9f8decf50449e2683a5aeb667a3
Petr Bělohlávek <petr.bel@centrum.cz>
2011-09-13 22:23:05 +0200

    spinner separation+development, early fetchEvents solved (callback)

---
+++ www/js/spinner.js
@@ -0,0 +1,56 @@
+/**
+ * Spinner
+ * @author Petr Bělohlávek
+ */
+
+/**
+ * Global functions
+ */
+jQuery.extend({
+	spinner: {
+		/**
+		 * Size of the spinner
+		 * @var integer
+		 */
+		size : 31,
+
+		/**
+		 * Returns object representing the spinner
+		 * @return object
+		 */
+		getSpinner : function () {
+			return $('<img class="spinner" />').attr('src', jQuery.gameMap.getBasepath() + '/images/spinner.gif').css({
+			'position' : 'absolute',
+			'z-index' : '999999999999999999999999999999999999999999999999999999999999'});
+		},
+
+		/**
+		 * Shows spinner
+		 * @param string/object
+		 * @return void
+		 */
+		show : function (target)
+		{
+			var spinnerClone = this.getSpinner().clone();
+
+			var targetWidth = $(target).css('width');
+			var targetHeight = $(target).css('height');
+
+			spinnerClone.css({
+				'top' : targetHeight.substring(0, targetHeight.length -2)/2 - this.size/2 + 'px',
+				'left' : targetWidth.substring(0, targetWidth.length -2)/2 - this.size/2 + 'px'
+			});
+			$(target).append(spinnerClone);
+		},
+
+		/**
+		 * Hides spinner
+		 * @return void
+		 */
+		hide : function ()
+		{
+			$('.spinner').remove();
+		},
+
+	}
+});
