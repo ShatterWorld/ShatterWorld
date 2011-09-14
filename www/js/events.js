9921f6e31f5721339e24202b98ced02285aa8aa4
Petr Bělohlávek <petr.bel@centrum.cz>
2011-09-14 22:24:47 +0200

    basic countdown formating

--- www/js/events.js
+++ www/js/events.js
@@ -17,7 +17,18 @@ jQuery.extend({
 				jQuery.countdown.cleanDialog();
 				var count = 0;
 				$.each(data['events'], function(key, event) {
-					jQuery.countdown.addCountdown('ahoj', event['countdown']);
+
+					var type = event['type'];
+					var label = 'Neznámá akce';
+
+					if (type == 'colonisation'){
+						label = 'Kolonizace [' + event['info']['target']['coordX'] + ';'+event['info']['target']['coordX'] + ']';
+					}
+					else if (type == 'facilityConstruction'){
+						label = event['info']['constructionType']+' (lvl '+event['info']['level']+') [' + event['info']['field']['coordX'] + ';'+event['info']['field']['coordX'] + ']';
+					}
+
+					jQuery.countdown.addCountdown(label, event['countdown']);
 					count++
 				});
 				if (count > 0){
