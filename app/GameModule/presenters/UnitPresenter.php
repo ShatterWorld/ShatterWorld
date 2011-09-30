4359170f542bf6d5dd8058f40f465d1b00521a68
Petr Bělohlávek <petr.bel@centrum.cz>
2011-09-30 21:13:35 +0200

    unitpresenter basics

---
+++ app/GameModule/presenters/UnitPresenter.php
@@ -0,0 +1,43 @@
+<?php
+namespace FrontModule;
+use Nette;
+use Nette\Application\UI\Form;
+class UnitPresenter extends BasePresenter
+{
+
+	protected function createComponentTrainUnitForm ()
+	{
+
+		$form = new Form;
+
+		$units = $this->context->rules->getAll('unit');
+
+		foreach ($units as $name => $unit){
+			$form->addText($name, $name);
+
+		}
+
+		$form->addSubmit('send', 'Trénovat');
+
+		//$form->setTranslator($translator);
+
+
+		$form->onSuccess[] = callback($this, 'submitTrainUnitForm');
+
+		return $form;
+	}
+
+	public function submitTrainUnitForm ($form)
+	{
+		/*$data = $form->getValues();
+		$data['role'] = 'player';
+		$this->getService('userService')->create($data);
+
+		$this->flashMessage("Vaše registrace proběhla úspěšně");
+
+		$this->redirect('Homepage:');*/
+
+	}
+
+
+}
