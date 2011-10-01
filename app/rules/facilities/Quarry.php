1bf5943208ee18554b1242b0a52bb37b35413e28
Jan Buchar <Teyras@gmail.com>
2011-10-01 21:58:36 +0200

    added buildings

---
+++ app/rules/facilities/Quarry.php
@@ -0,0 +1,41 @@
+<?php
+namespace Rules\Facilities;
+use Rules\AbstractRule;
+
+class Quarry extends AbstractRule implements IFacility
+{
+	public function getDependencies ()
+	{
+		return array();
+	}
+	
+	public function getConstructionCost ($level = 1)
+	{
+		return array(
+			'stone' => $level * 5,
+			'metal' => ($level - 1) * 10 
+		);
+	}
+	
+	public function getConstructionTime ($level = 1)
+	{
+		return pow($level, 2) * 90;
+	}
+	
+	public function getDemolitionCost ($from, $level = 0)
+	{
+		return array();
+	}
+	
+	public function getDemolitionTime ($from, $level = 0)
+	{
+		return ($from - $level) * 90;
+	}
+	
+	public function getProduction ($level = 1)
+	{
+		return array(
+			'stone' => $level / 100
+		);
+	}
+}
