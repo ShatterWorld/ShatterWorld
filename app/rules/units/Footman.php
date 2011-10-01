6464afe270d4c1e1676365380ff6b9c30525203a
Jan Buchar <Teyras@gmail.com>
2011-10-01 19:05:45 +0200

    new unit types

---
+++ app/rules/units/Footman.php
@@ -0,0 +1,48 @@
+<?php
+namespace Rules\Units;
+use Rules\AbstractRule;
+
+class Footman extends AbstractRule implements IUnit
+{
+	public function getAttack ()
+	{
+		return 2;
+	}
+	
+	public function getDefense ()
+	{
+		return 3;
+	}
+	
+	public function getSpeed ()
+	{
+		return 100;
+	}
+	
+	public function getCost ()
+	{
+		return array(
+			'food' => 60,
+			'metal' => 30
+		);
+	}
+	
+	public function getUpkeep ()
+	{
+		return array(
+			'food' => 0.005
+		);
+	}
+	
+	public function getTrainingTime ()
+	{
+		return 3600;
+	}
+	
+	public function getDifficulty ()
+	{
+		return array(
+			'barracks' => 1
+		);
+	}
+}
---
+++ app/rules/units/HeavyVehicle.php
@@ -0,0 +1,50 @@
+<?php
+namespace Rules\Units;
+use Rules\AbstractRule;
+
+class HeavyVehicle extends AbstractRule implements IUnit
+{
+	public function getAttack ()
+	{
+		return 7;
+	}
+	
+	public function getDefense ()
+	{
+		return 5;
+	}
+	
+	public function getSpeed ()
+	{
+		return 150;
+	}
+	
+	public function getCost ()
+	{
+		return array(
+			'food' => 60,
+			'metal' => 150
+		);
+	}
+	
+	public function getUpkeep ()
+	{
+		return array(
+			'food' => 0.02,
+			'fuel' => 0.03
+		);
+	}
+	
+	public function getTrainingTime ()
+	{
+		return 7200;
+	}
+	
+	public function getDifficulty ()
+	{
+		return array(
+			'barracks' => 2,
+			'workshop' => 6
+		);
+	}
+}
