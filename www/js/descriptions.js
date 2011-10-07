2841f6bfba21ae71382d8834caf880f38d70e120
Petr Bělohlávek <petr.bel@centrum.cz>
2011-10-07 17:13:11 +0200

    lock

--- www/js/descriptions.js
+++ www/js/descriptions.js
@@ -1,19 +1,29 @@
 jQuery.extend({
 	descriptions: {
 		data: null,
-		
+
+		period : 250,
+
 		fetchDescriptions: function () {
 			$.get('?do=fetchDescriptions', function (data) {
 				jQuery.descriptions.data = data['descriptions'];
 			})
 		},
-		
+
 		get: function (type, key) {
-			if (this.data !== null && typeof(this.data[type][key]) !== 'undefined') {
+			if (this.data === null){
+				setTimeout(function(){
+					return jQuery.descriptions.get(type, key);
+				}, jQuery.descriptions.period);
+			}
+
+			if (typeof(this.data[type][key]) !== 'undefined') {
 				return this.data[type][key];
 			} else {
 				return key;
 			}
+
+
 		}
 	}
 });
