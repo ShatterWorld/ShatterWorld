930f38c3a1abc9ab05229325dc83803387baa48e
Petr Bělohlávek <petr.bel@centrum.cz>
2011-10-07 19:17:26 +0200

    eh... almost locked

--- www/js/descriptions.js
+++ www/js/descriptions.js
@@ -4,27 +4,61 @@ jQuery.extend({
 
 		period : 250,
 
+		fetching : false,
+
 		fetchDescriptions: function () {
+			this.fetching = true;
 			$.get('?do=fetchDescriptions', function (data) {
 				jQuery.descriptions.data = data['descriptions'];
+				jQuery.descriptions.fetching = false;
 			})
 		},
 
 		get: function (type, key) {
-			if (this.data === null){
-				setTimeout(function(){
-					return jQuery.descriptions.get(type, key);
-				}, jQuery.descriptions.period);
-			}
+			/*while (this.fetching){
+				this.wait();
+			}*/
 
-			if (typeof(this.data[type][key]) !== 'undefined') {
+			if (this.data !== null && typeof(this.data[type][key]) !== 'undefined') {
 				return this.data[type][key];
 			} else {
 				return key;
 			}
 
+		},
 
+		wait : function(time, type){
+			time = time || 1000;
+			type = type || "fx";
+			return this.queue(type, function() {
+				var self = this;
+				setTimeout(function() {
+					$(self).dequeue();
+				}, time);
+			});
 		}
+
+
+/*    $.fn.wait = function(time, type) {
+        time = time || 1000;
+        type = type || "fx";
+        return this.queue(type, function() {
+            var self = this;
+            setTimeout(function() {
+                $(self).dequeue();
+            }, time);
+        });
+    };
+    function runIt() {
+      $("div").wait()
+              .animate({left:'+=200'},2000)
+              .wait()
+              .animate({left:'-=200'},1500,runIt);
+    }
+    runIt();*/
+
+
+
 	}
 });
 
