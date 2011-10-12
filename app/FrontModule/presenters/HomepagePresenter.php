786878e231d75095ba121394a9037c4050866f40
petrbel <petr.bel@centrum.cz>
2011-10-12 15:21:54 +0200

    front layout

--- app/FrontModule/presenters/HomepagePresenter.php
+++ app/FrontModule/presenters/HomepagePresenter.php
@@ -1,46 +1,4 @@
 <?php
 namespace FrontModule;
-use Nette;
-use Nette\Application\UI\Form;
-use Nette\Security\IAuthenticator;
 
-class HomepagePresenter extends BasePresenter {
-	protected function createComponentLoginForm ()
-	{
-		$form = new Form;
-
-		$form->addText('login', 'Login', 13)
-			->setRequired('Zadejte prosím přihlašovací jméno');
-
-		$form->addPassword('password', 'Heslo', 13)
-			->setRequired('Zadejte prosím heslo');
-
-		$form->addSubmit('send', 'Přihlásit');
-
-		//$form->setTranslator($translator);
-
-		$form->onSuccess[] = callback($this, 'submitLoginForm');
-
-		return $form;
-	}
-
-	public function submitLoginForm ($form)
-	{
-		$data = $form->getValues();
-		try {
-			$this->getUser()->login($data["login"], $data["password"]);
-		}
-		catch (Nette\Security\AuthenticationException $e) {
-			if ($e->getCode() === IAuthenticator::INVALID_CREDENTIAL) {
-				$this->flashMessage("Špatné heslo", "error");
-			} elseif ($e->getCode() === IAuthenticator::IDENTITY_NOT_FOUND) {
-				$this->flashMessage("Špatný login", "error");
-			} else {
-				$this->flashMessage("Neočekávaná chyba", "error");
-			}
-			$this->redirect('this');
-		}
-		$this->flashMessage("Úspěšně přihlášen");
-		$this->redirect(":Game:Dashboard:");
-	}
-}
+class HomepagePresenter extends BasePresenter {}
--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -1,5 +1,7 @@
 <?php
 use Nette\Utils\Strings;
+use Nette\Application\UI\Form;
+use Nette\Security\IAuthenticator;
 
 /**
  * Base class for all application presenters.
@@ -15,4 +17,45 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 		}
 		return parent::__call($name, $args);
 	}
+
+	protected function createComponentLoginForm ()
+	{
+		$form = new Form;
+
+		$form->addText('login', 'Login', 13)
+			->setRequired('Zadejte prosím přihlašovací jméno');
+
+		$form->addPassword('password', 'Heslo', 13)
+			->setRequired('Zadejte prosím heslo');
+
+		$form->addSubmit('send', 'Přihlásit');
+
+		//$form->setTranslator($translator);
+
+		$form->onSuccess[] = callback($this, 'submitLoginForm');
+
+		return $form;
+	}
+
+	public function submitLoginForm ($form)
+	{
+		$data = $form->getValues();
+		try {
+			$this->getUser()->login($data["login"], $data["password"]);
+		}
+		catch (Nette\Security\AuthenticationException $e) {
+			if ($e->getCode() === IAuthenticator::INVALID_CREDENTIAL) {
+				$this->flashMessage("Špatné heslo", "error");
+			} elseif ($e->getCode() === IAuthenticator::IDENTITY_NOT_FOUND) {
+				$this->flashMessage("Špatný login", "error");
+			} else {
+				$this->flashMessage("Neočekávaná chyba", "error");
+			}
+			$this->redirect('this');
+		}
+		$this->flashMessage("Úspěšně přihlášen");
+		$this->redirect(":Game:Dashboard:");
+	}
+
+
 }
