b3623865b33059b3d84b9bb1314755d9a225b265
Jan Buchar <Teyras@gmail.com>
2011-11-28 17:48:37 +0100

    tooltips refactoring

---
+++ www/js/tooltips.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+	var set = $('.hasTooltip');
+	var getTooltipElement = function (selector) {
+		return $('.tooltip[data-for="' + $(selector).attr('id') + '"]');
+	};
+	$(set).live('mouseenter', function (e) {
+		getTooltipElement(this).css({
+			display: 'block',
+			left: e.pageX + 10,
+			top: e.pageY + 20
+		});
+	});
+	$(set).live('mousemove', function (e) {
+		getTooltipElement(this).css({
+			left: e.pageX + 10,
+			top: e.pageY + 20
+		});
+	});
+	$(set).live('mouseleave', function (e) {
+		getTooltipElement(this).css({
+			display: 'none'
+		});
+	});
+});
