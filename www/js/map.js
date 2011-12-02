8633dc3e594e494d7407a19730300ab3eb5bbaf7
Jan Buchar <Teyras@gmail.com>
2011-12-02 20:35:19 +0100

    added placeholder facility images

--- www/js/map.js
+++ www/js/map.js
@@ -385,11 +385,8 @@ Game.map = {
 					}
 
 					if (field['owner'] !== null && Game.map.clan !== null && field['owner']['id'] == Game.map.clan){
-						if (field['facility'] !== null && field['facility'] == 'headquarters'){
+						if (field['facility'] !== null){
 							var img = $('<img src="' + basePath + '/images/facilities/' + field['facility'] + '.png"/>');
-							img.css({
-								'margin' : '10px'
-							});
 							div.append(img);
 						}
 					}
