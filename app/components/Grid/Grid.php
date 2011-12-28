50886348e6a76fcca3c538e0c8287450b547f7f6
petrbel <petr.bel@centrum.cz>
2011-12-28 12:39:23 +0100

    report loot icons

--- app/components/Grid/Grid.php
+++ app/components/Grid/Grid.php
@@ -2,15 +2,18 @@
 class Grid extends Nette\Application\UI\Control
 {
 	protected $templateFile = 'grid.latte';
-	
+
 	protected $columns;
-	
+
+	protected $name;
+
 	public function __construct (Nette\ComponentModel\IContainer $parent = NULL, $name = NULL, $columns = array())
 	{
 		parent::__construct($parent, $name);
 		$this->columns = $columns;
+		$this->name = $name;
 	}
-	
+
 	public function render ($data, $heading = NULL)
 	{
 		$template = $this->getTemplate();
@@ -18,6 +21,7 @@ class Grid extends Nette\Application\UI\Control
 		$template->heading = $heading;
 		$template->columns = $this->columns;
 		$template->data = $data;
+		$template->type = $this->name;
 		$template->render();
 	}
 }
