95906e450c2431dadf555bda5674c949480a426f
petrbel <petr.bel@centrum.cz>
2012-01-01 20:32:34 +0100

    reports, unit fix

---
+++ www/js/reports.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	$(".report .reportBody").hide();
+
+	$(".report .reportHead").click(function(){
+		var report = $(this).parent();
+		report.children(".reportBody").toggle("fast");
+
+		var params = {'reportId': report.attr('id')};
+
+		if (report.attr('class') == 'report unread'){
+			Game.utils.signal('markRead', params);
+			report.attr('class', 'report read');
+		}
+	});
+
+});
