636f0270e71c386d849335a3fdb7775f73878e11
petrbel <petr.bel@centrum.cz>
2012-01-09 16:26:13 +0100

    investigation report

--- app/rules/events/Investigation.php
+++ app/rules/events/Investigation.php
@@ -15,19 +15,16 @@ class Investigation extends Spy implements IEvent
 
 	public function getExplanation (Entities\Event $event)
 	{
-		return sprintf('Špionáž  %s', $event->target->owner->name);
+		return sprintf('Špionáž  klanu %s na poli %s', $event->target->owner->name, $event->target->getCoords());
 	}
 
 	protected function evaluateSpy (Entities\Event $event)
 	{
 		$result = array(
 			'successful' => FALSE,
+			'targetCoords' => '',
 			'researches' => array(),
-			'facility' => array(
-				'type' => '',
-				'level' => 0
-			),
-			'quests' => array(),
+			'resources' => array(),
 			'attacker' => array(
 				'units' => array(),
 				'casualties' => array()
@@ -65,15 +62,21 @@ class Investigation extends Spy implements IEvent
 		$result['attacker']['casualties'] = array('spy' => intval(round($attackerSpyCount * $attackerCasualtiesCoefficient)));
 		$result['defender']['casualties'] = array('spy' => intval(round($defenderSpyCount * $defenderCasualtiesCoefficient)));
 
+		$result['targetCoords'] = $event->target->getCoords();
 		if ($attackerPower > $defenderPower){
 			$result['successful'] = TRUE;
-			$result['field']['type'] = $event->target->type;
-			$result['field']['facility']['type'] = $event->target->facility ? $event->target->facility->type : 'žádná';
-			$result['field']['facility']['level'] = $event->target->facility ? $event->target->facility->level : '0';
+			if ($event->target->facility){
+				$result['facility'] = array();
+				$result['facility']['type'] = $event->target->facility->type;
+				$result['facility']['level'] = $event->target->facility->level;
+
+			}
 			$researches = $this->getContext()->model->getResearchRepository()->getResearched($event->target->owner);
 			foreach ($researches as $research){
 				$result['researches'][$research->type] = $research->level;
 			}
+			$result['territorySize'] = $this->getContext()->model->getFieldRepository()->getTerritorySize($event->target->owner);
+			$result['resources'] = $this->getContext()->model->getResourceRepository()->getResourcesArray($event->target->owner);
 		}
 
 		return $result;
@@ -86,7 +89,17 @@ class Investigation extends Spy implements IEvent
 		$message = '';
 		if ($report->type === 'owner') {
 			if ($data['successful']) {
-				$message = array(ReportItem::create('text', 'Akce naší rozvědky se zdařila!'));
+				$message = array(ReportItem::create('text', 'Špionáž se naší rozvědce zdařila!'));
+
+				if (isset($data['facility'])){
+					$message[] = ReportItem::create('text', 'Na poli ' . $data['targetCoords'] . ' stojí ' . $this->getContext()->rules->get('facility', $data['facility']['type'])->getDescription() . ' (úroveň ' . $data['facility']['level'] . ')');
+				}
+				else{
+					$message[] = ReportItem::create('text', 'Na poli ' . $data['targetCoords'] . ' nestojí žádná budova.');
+				}
+
+				$message[] = ReportItem::create('text', 'Rozloha klanu: ' . $data['territorySize']);
+
 				$message[] = ReportItem::create('unitGrid', array(
 						DataRow::from($data['attacker']['units'])->setLabel('Jednotky'),
 						DataRow::from($data['attacker']['casualties'])->setLabel('Ztráty')
@@ -100,14 +113,45 @@ class Investigation extends Spy implements IEvent
 				$message[] = ReportItem::create('researchGrid', array(
 						DataRow::from($data['researches'])->setLabel('Výzkumy')
 					))->setHeading('Výzkumy');
+
+				$balance = array();
+				$production = array();
+				$storage = array();
+				foreach($data['resources'] as $name => $resource){
+					$balance[$name] = floor($resource['balance']);
+					$production[$name] = floor($resource['production'] * 3600);
+					$storage[$name] = floor($resource['storage']);
+				}
+				$message[] = ReportItem::create('resourceGrid', array(
+						DataRow::from($balance)->setLabel('Ve skladech'),
+						DataRow::from($production)->setLabel('Produkce'),
+						DataRow::from($storage)->setLabel('Kapacita skladů')
+					))->setHeading('Suroviny');
 			} else {
 				$message = array(ReportItem::create('text', 'Akce naší rozvědky se nezdařila! Měl bys posílit řady špionů!'));
+				$message[] = ReportItem::create('unitGrid', array(
+						DataRow::from($data['attacker']['units'])->setLabel('Jednotky'),
+						DataRow::from($data['attacker']['casualties'])->setLabel('Ztráty')
+					))->setHeading('Útočník');
 			}
 		} else {
 			if ($data['successful']) {
 				$message = array(ReportItem::create('text', 'Nepřítel infiltroval tvůj klan! Měl bys posílit řady špionů!'));
+				$message[] = ReportItem::create('unitGrid', array(
+						DataRow::from($data['defender']['units'])->setLabel('Jednotky'),
+						DataRow::from($data['defender']['casualties'])->setLabel('Ztráty'),
+					))->setHeading('Obránce');
 			} else {
 				$message = array(ReportItem::create('text', 'Nepříteli se nepodařilo infiltrovat tvůj klan!'));
+				$message[] = ReportItem::create('unitGrid', array(
+						DataRow::from($data['attacker']['units'])->setLabel('Jednotky'),
+						DataRow::from($data['attacker']['casualties'])->setLabel('Ztráty')
+					))->setHeading('Útočník');
+
+				$message[] = ReportItem::create('unitGrid', array(
+						DataRow::from($data['defender']['units'])->setLabel('Jednotky'),
+						DataRow::from($data['defender']['casualties'])->setLabel('Ztráty'),
+					))->setHeading('Obránce');
 			}
 		}
 		return $message;
